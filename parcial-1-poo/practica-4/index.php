958689eaf121cee95eaf73a53144cbdfeb877bcf
SebastianGarcia <gilbertoseba1041@gmail.com>
2026-03-07 23:57:19 -0700

    Agregar index.php y README

---
+++ parcial-1-poo/practica-4/index.php
@@ -0,0 +1,41 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require_once 'clases/Admin.php';
+require_once 'clases/Alumno.php';
+require_once 'clases/Invitado.php';
+
+$usuarios = [];
+
+try {
+    $usuarios [] = new Admin ("Ines Gil", "ines.admin@uas.edu.mx");
+    $usuarios [] = new Alumno("Maria Solorzano", "maria.alumno@uas.edu.mx", "A12345");
+    $usuarios [] = new Invitado("Sebastian Garcia", "sebastian.invitado@xempresa.com", "XEmpresa");
+    $usuarios[] = new Alumno("Error", "Correo-invalido", "B6789");
+} catch (Exception $e) {
+    echo "Error controlado: ". $e->getMessage();
+}
+
+echo "<table border='1' cellpadding='8' cellspacing='0'>";
+echo "<tr><th>Nombre</th><th>Correo</th><th>Rol</th><th>Matricula</th><th>Empresa</th></tr>";
+
+foreach ($usuarios as $usuario) {
+    echo "<tr>";
+    echo "<td>" . $usuario->getNombre() . "</td>";
+    echo "<td>" . $usuario->getCorreo() . "</td>";
+    echo "<td>" . $usuario->getRol() . "</td>";
+
+    echo "<td>";
+    echo method_exists($usuario, 'getMatricula') ? $usuario->getMatricula() : "—";
+    echo "</td>";
+
+    echo "<td>";
+    echo method_exists($usuario, 'getEmpresa') ? $usuario->getEmpresa() : "—";
+    echo "</td>";
+
+    echo "</tr>";
+}
+echo "</table>";
+?>
+
